4a4ba6118c192ac8d383bb2aa993ef52192f1867
Mahmoud Feshar <104142801+mahfeshar@users.noreply.github.com>
2024-07-21 15:11:03 +0300

    [217] [Time Beats: 36.90%] [Memory Beats: 43.04%] - LeetPush

---
+++ Neetcode-150/217-Contains-Duplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        mySet = set()
+        for i in nums:
+            if i in mySet:
+                return True
+            mySet.add(i)
+        return False
+            
